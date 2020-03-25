cc7917d4db9f786b5c4ff835212f1c5619dabfb7
Jake Johnson <jakejjohnson@gmail.com>
2020-03-25 10:10:46 -0500

    testing update

--- src/tests/Episodes.test.js
+++ src/tests/Episodes.test.js
@@ -3,7 +3,12 @@ import { render } from '@testing-library/react';
 import Episodes from '../components/Episodes';
 //Episode test
 test('Data rendered', () => {
-    const {} = render(
-        <Episodes error='' episodes={[]} />
+    const { queryByTestId } = render(
+        <Episodes error='' episodes={episodesTester} />
     )
+    expect(queryByTestId(/Card1/i)).toHaveLength(2)    
 })
+
+const episodesTester = [{id: 909340, url: "http://www.tvmaze.com/episodes/909340/stranger-things-2x01-chapter-one-madmax", name: "Chapter One: MADMAX", season: 2, number: 1}
+, {id: 909342, url: "http://www.tvmaze.com/episodes/909342/stranger-things-2x02-chapter-two-trick-or-treat-freak", name: "Chapter Two: Trick or Treat, Freak", season: 2, number: 2}
+]
